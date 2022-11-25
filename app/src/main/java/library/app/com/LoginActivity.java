e1dc3c8da2586dc06e19e7b473ae0582fab25e07
daff00 <daffa.kaisha@student.umn.ac.id>
2022-11-25 14:27:22 +0700

    tes

--- app/src/main/java/library/app/com/LoginActivity.java
+++ app/src/main/java/library/app/com/LoginActivity.java
@@ -31,7 +31,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
-
+    //.
     //Variable
     EditText txtEmail;
     EditText txtPassword;
