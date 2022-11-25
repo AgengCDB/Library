4ad066097cfd028d614221b01f18c911efda68b6
daff00 <daffa.kaisha@student.umn.ac.id>
2022-11-25 21:05:44 +0700

    Tambahan activity profile + change password

---
+++ app/src/main/java/library/app/com/ProfileActivity.java
@@ -0,0 +1,47 @@
+package library.app.com;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    EditText edtUsername, edtEmail, edtPhone, edtPassword;
+    Button btnEdit, btnHapus, btnChangePw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        edtUsername = (EditText) findViewById(R.id.edtUsername);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtPhone = (EditText) findViewById(R.id.edtPhoneNumber);
+
+        btnEdit = (Button) findViewById(R.id.btnEdit);
+        btnHapus = (Button) findViewById(R.id.btnHapus);
+        btnChangePw = (Button) findViewById(R.id.btnChangePassword);
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btnHapus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btnChangePw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ChangePasswordActivity.class));
+            }
+        });
+    }
+}
