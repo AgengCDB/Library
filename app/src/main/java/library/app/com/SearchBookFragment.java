cb7d2e8e92dd6d10e16775e26554a93b6c76ca84
daff00 <daffa.kaisha@student.umn.ac.id>
2022-12-08 09:30:07 +0700

    Update SearchBookFragment.java

--- app/src/main/java/library/app/com/SearchBookFragment.java
+++ app/src/main/java/library/app/com/SearchBookFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -63,6 +64,7 @@ public class SearchBookFragment extends Fragment {
 
         list_book = new ArrayList<>();
         lv = v.findViewById(R.id.listView);
+        btnSearch = v.findViewById(R.id.btnSearch);
 
         RequestQueue queue = Volley.newRequestQueue(getContext());
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url_get_book, new Response.Listener<String>() {
@@ -123,6 +125,100 @@ public class SearchBookFragment extends Fragment {
             }
         });
         queue.add(stringRequest);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                list_book.clear();
+                //Adding Button Timeout Avoiding Application Bug
+                //Bug -> Repeating when repeat button click event
+                btnSearch.setEnabled(false);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        btnSearch.setEnabled(true);
+                    }
+                }, 2000);
+                //Get Text
+                txtSearch = v.findViewById(R.id.search_book);
+                user_search_input = txtSearch.getText().toString();
+
+                RequestQueue queue = Volley.newRequestQueue(getContext());
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url_search_book, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+
+                            JSONObject jObj = new JSONObject(response);
+                            JSONArray member = jObj.getJSONArray("data");
+
+                            for (int i = 0; i < member.length(); i++) {
+                                JSONObject a = member.getJSONObject(i);
+                                String id = a.getString(TAG_ID);
+                                String title = a.getString(TAG_TITLE);
+                                String author = a.getString(TAG_AUTHOR);
+                                String type = a.getString(TAG_TYPE);
+
+//                        Log.e("JSON", title + "||" + author + "||" + type);
+
+                                HashMap<String, String> map = new HashMap<>();
+                                map.put("id", id);
+                                map.put("title", title);
+                                map.put("author",author);
+                                map.put("type", type);
+
+                                list_book.add(map);
+                                String[] from = {"title", "author", "type"};
+                                int[] to = {R.id.txtJudul, R.id.txtAuthor, R.id.txtType};
+
+                                ListAdapter adapter = new SimpleAdapter(
+                                        getContext(), list_book, R.layout.list_book,
+                                        from, to);
+                                lv.setAdapter(adapter);
+                                lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                                    @Override
+                                    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+//                                String nomor = list_book.get(position).get(TAG_ID);
+//                                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+//                                i.putExtra("id", nomor);
+//                                startActivity(i);
+//
+                                        return true;
+                                    }
+                                });
+                            }
+                        }
+                        catch (Exception ex) {
+                            Log.e("Error", ex.toString());
+//                    progressBar2.setVisibility(View.GONE);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Errr", error.getMessage());
+                        Toast.makeText(getContext(), "silahkan cek koneksi internet anda", Toast.LENGTH_SHORT).show();
+                        getActivity().finish();
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("user_input", user_search_input);
+                        return params;
+                    }
+
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("Content-Type", "application/x-www-form-urlencoded");
+                        return params;
+                    }
+                };
+                queue.getCache().clear();
+                queue.add(stringRequest);
+            }
+        });
 
         return v;
     }
