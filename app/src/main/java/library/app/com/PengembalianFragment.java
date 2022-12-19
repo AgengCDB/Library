f814914711cc96719e83c65574fbd9899388048c
AgengCDB <ageng.widjaya7@gmail.com>
2022-12-19 10:35:31 +0700

    Benerin margin

--- app/src/main/java/library/app/com/PengembalianFragment.java
+++ app/src/main/java/library/app/com/PengembalianFragment.java
@@ -12,10 +12,21 @@ import androidx.fragment.app.Fragment;
 
 public class PengembalianFragment extends Fragment {
 
+    private static final String TAG_ID="id";
+    private static final String TAG_TITLE="title";
+    private static final String TAG_AUTHOR="author";
+    private static final String TAG_TYPE="type";
+    private static final String TAG_PAGES="pages";
+    private static final String TAG_ISBN="isbn";
+    private static final String TAG_BORROWED="borrowed";
+    private static final String TAG_STATUS="status";
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.activity_pengembalian_fragment, container, false);
+
+
 
         return v;
     }
