7f3e11f1370b492abf846135edb61cdca5b141cf
AgengCDB <ageng.widjaya7@gmail.com>
2022-12-19 11:10:48 +0700

    form pengembalian

--- app/src/main/java/library/app/com/FormPengembalianActivity.java
+++ app/src/main/java/library/app/com/FormPengembalianActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.HashMap;
+
 public class FormPengembalianActivity extends AppCompatActivity {
+
+    SessionManager sessionManager;
 
     private static final String TAG_ID="id";
     private static final String TAG_TITLE="title";
@@ -19,5 +23,14 @@ public class FormPengembalianActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_pengembalian);
+
+        sessionManager = new SessionManager(FormPengembalianActivity.this);
+        sessionManager.checkLogin();
+
+        HashMap<String, String> user = sessionManager.getUserDetail();
+        String userid = user.get(sessionManager.ID_USER);
+        String username = user.get(sessionManager.USERNAME);
+        String email = user.get(sessionManager.EMAIL);
+        String phone = user.get(sessionManager.PHONE);
     }
 }
